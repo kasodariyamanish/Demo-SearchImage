dca1180cdb49ea783274df6232cc3f3f980da9f8
Manish <kasodariyamanish@gmail.com>
2020-11-28 10:37:06 +0530

    -- Make custome class for store global variables.

---
+++ Demo-SearchImage/CustomClass/GlobalVars.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalVars.swift
+//  Demo-SearchImage
+//
+//  Created by Manish's Mac on 28/11/20.
+//
+
+import UIKit
+
+class Keys {
+    static let PIXABAY_API_KEY = "19299666-5492e41c94484d36d6c56676f"
+}
+
