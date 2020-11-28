5c9b2ac2b52b5428082865e930e024dd67c6972f
Manish <kasodariyamanish@gmail.com>
2020-11-28 10:41:31 +0530

    -- Made service and list model class

---
+++ Demo-SearchImage/CustomClass/ServiceCalss.swift
@@ -0,0 +1,12 @@
+//
+//  ServiceCalss.swift
+//  Demo-SearchImage
+//
+//  Created by Manish's Mac on 28/11/20.
+//
+
+import UIKit
+
+class ServiceCalss {
+    
+}
---
+++ Demo-SearchImage/ImageSearchViews/SearchListModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchListModel.swift
+//  Demo-SearchImage
+//
+//  Created by Manish's Mac on 28/11/20.
+//
+
+import Foundation
+
+struct SearchListModel: Codable {
+
+}
